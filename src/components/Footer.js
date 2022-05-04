0d1dea4f8be5ea9f4deeb2b47b7bd89ba5f23436
jwputah <jwputah7@gmail.com>
2022-05-04 10:44:03 -0600

    navbar and footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Footer() {
     return (
-        <section className="my-5" >
-            <div className="my-2">
-                <li>
-                <a href="https://github.com/jwputah"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
-                <a href="mailto:jwputah7@gmail.com"><img src="https://www.freepnglogos.com/uploads/email-png/blue-email-box-circle-png-transparent-icon-2.png" width="38" alt="Email from freepnglogos.com" className="icon"/></a>
-                </li>
+        <footer>
+            <div class="container">
+                <div class="row justify-content-start">
+                    <div class="col">
+                        <h1 style={{
+                        textAlign: "left"
+                        }}>
+                        <a href="https://github.com/jwputah">
+                            <img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon" />
+                            </a>
+                        </h1>
+                    </div>
+
+                    <div class="col">
+                        <h1 style={{
+                        color: "green",
+                        textAlign: "center"
+                        }}>Joshua W Pace</h1>
+                    </div>
+
+                    <div class="col">
+                        <h1 style={{
+                        textAlign: "right"
+                        }}>
+                        <a href="mailto:jwputah7@gmail.com">
+                            <img src="https://www.freepnglogos.com/uploads/email-png/blue-email-box-circle-png-transparent-icon-2.png" width="38" alt="Email from freepnglogos.com" className="icon" />
+                            </a>
+                        </h1>
+                    </div>
+
+                </div>
             </div>
-        </section>
+        </footer>
     )
 }
 
