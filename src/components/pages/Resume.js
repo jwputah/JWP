ce8004f5cda3b7ac55572b62757d42e60dfa19de
jwputah <jwputah7@gmail.com>
2022-05-11 15:50:09 -0600

    added deployed links

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -22,7 +22,7 @@ export default function Resume() {
                     <div style={{
                         textAlign: "center",
                     }}>
-                    <a href="/">
+                    <a href="https://docs.google.com/document/d/1EyCFNYi5ot-NslHHFD0N1ox_-ATZpWcpNQilnUw8SG4/edit?usp=sharing">
                     <img src="https://img.icons8.com/ios-filled/50/000000/open-resume.png" alt="Resume" className="icon" /></a>
                     </div>
                 </div>
