bfdda1003eebd73b5474a879191123bbac58994a
jwputah <jwputah7@gmail.com>
2022-07-14 15:34:29 -0600

    added emailjscom to contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
+import emailjs from 'emailjs-com'
 import '../../assets/css/contact.css';
-
-// Here we import a helper function that will check if the email is valid
-import { checkPassword, validateEmail } from '../../utils/helpers';
 
 const contactBg = require("../../../src/assets/images/airplane.gif");
 const divStyle = {
@@ -13,93 +11,114 @@ const divStyle = {
     backgroundSize: 'cover'
 };
 
-function Form() {
+const Form = () => {
     // Create state variables for the fields in the form
     // We are also setting their initial values to an empty string
     const [email, setEmail] = useState('');
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+    const [emailSent, setEmailSent] = useState(false);
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
 
-    const handleInputChange = (e) => {
-        // Getting the value and name of the input which triggered the change
-        const { target } = e;
-        const inputType = target.name;
-        const inputValue = target.value;
+    const submit = () => {
+        if (name && email && message) {
+            const serviceId = 'service_9vs54i5';
+            const templateId = 'template_1jai5td';
+            const userId = 'bCT8fHVLV25d9PuvY';
+            const templateParams = {
+                name,
+                email,
+                message
+            };
 
-        // Based on the input type, we set the state of either email, username, and password
-        if (inputType === 'email') {
-            setEmail(inputValue);
-        } else if (inputType === 'userName') {
-            setUserName(inputValue);
+            emailjs.send(serviceId, templateId, templateParams, userId)
+                .then(response => console.log(response))
+                .then(error => console.log(error));
+
+            setName('');
+            setEmail('');
+            setMessage('');
+            setEmailSent(true);
         } else {
-            setPassword(inputValue);
+            alert('Please fill in all fields.');
         }
+
+        // const handleInputChange = (e) => {
+        //     // Getting the value and name of the input which triggered the change
+        //     const { target } = e;
+        //     const inputType = target.name;
+        //     const inputValue = target.value;
+
+        //     // Based on the input type, we set the state of either email, username, and password
+        //     if (inputType === 'email') {
+        //         setEmail(inputValue);
+        //     } else if (inputType === 'name') {
+        //         setName(inputValue);
+        //     } else {
+        //         setMessage(inputValue);
+        //     }
     };
 
-    const handleFormSubmit = (e) => {
-        // Preventing the default behavior of the form submit (which is to refresh the page)
-        e.preventDefault();
+    // const handleFormSubmit = (e) => {
+    //     // Preventing the default behavior of the form submit (which is to refresh the page)
+    //     e.preventDefault();
 
-        // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-        if (!validateEmail(email) || !userName) {
-            setErrorMessage('Email or username is invalid');
-            // We want to exit out of this code block if something is wrong so that the user can correct it
-            return;
-            // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-        }
-        if (!checkPassword(password)) {
-            setErrorMessage(
-                `Choose a more secure password for the account: ${userName}`
-            );
-            return;
-        }
-        alert(`Hello ${userName}`);
+    //     // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    //     if (!validateEmail(email) || !name) {
+    //         setErrorMessage('Email or username is invalid');
+    //         // We want to exit out of this code block if something is wrong so that the user can correct it
+    //         return;
+    //         // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    //     }
+    //     if (!checkPassword(password)) {
+    //         setErrorMessage(
+    //             `Choose a more secure password for the account: ${name}`
+    //         );
+    //         return;
+    //     }
+    //     alert(`Hello ${userName}`);
 
-        // If everything goes according to plan, we want to clear out the input after a successful registration.
-        setUserName('');
-        setPassword('');
-        setEmail('');
-    };
+    //     // If everything goes according to plan, we want to clear out the input after a successful registration.
+    //     setUserName('');
+    //     setPassword('');
+    //     setEmail('');
+    // };
 
     return (
         <div style={divStyle}>
             <div className='Contact'>
-                <form className="form">
-                    <p className="text">Hello {userName}. Please fill out form and submit. Thank You!</p>
+                {emailSent ? (
+                    <span>Thank you for your email, I will be in touch as soon as possible!</span>
+                ) : (
+                    <form className="form">
+                    <p className="text">Hello. Please fill out form and submit. Thank You!</p>
                     <input
                         value={email}
                         name="email"
-                        onChange={handleInputChange}
+                        onChange={e => setEmail(e.target.value)}
                         type="email"
-                        placeholder="email"
+                        placeholder="Email"
                     />
                     <input
-                        value={userName}
-                        name="userName"
-                        onChange={handleInputChange}
+                        value={name}
+                        name="name"
+                        onChange={e => setName(e.target.value)}
                         type="text"
-                        placeholder="username"
+                        placeholder="Name"
                     />
-                    <input
-                        value={password}
-                        name="password"
-                        onChange={handleInputChange}
-                        type="password"
-                        placeholder="Password"
+                    <textarea
+                        value={message}
+                        name="message"
+                        onChange={e => setMessage(e.target.value)}
+                        type="text"
+                        placeholder="Message"
                     />
                     <div className='button'>
-                        <button type="button" onClick={handleFormSubmit}>Submit</button>
+                        <button type="button" onClick={submit}>Send Email!</button>
                     </div>
                 </form>
-                {errorMessage && (
-                    <div>
-                        <p className="error-text">{errorMessage}</p>
-                    </div>
                 )}
             </div>
         </div>
-
     );
 }
 
