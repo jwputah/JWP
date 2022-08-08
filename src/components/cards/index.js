8144e7249b8cabdc0e5efb0040f36e5aa97530c8
jwputah <jwputah7@gmail.com>
2022-08-07 20:02:27 -0600

    added padding to navbar and removed bullet on project page

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -10,9 +10,9 @@ function ProjectCards(props) {
             <div className="card-body">
                 <h5 className="card-title">{props.name}</h5>
             </div>
-                <ul className="list-group list-group-flush">
-                <li className="tools">{props.tools}</li>
-                </ul>
+                <div className="list-group list-group-flush">
+                <div className="tools">{props.tools}</div>
+                </div>
             <div className="card-body">
                 <a href={props.github} target="_blank" rel="noopener noreferrer"><img className="project-icon" src="https://img.icons8.com/ios-filled/50/000000/github.png" alt="GitHub Repo" /></a>
                 <a href={props.deploy} target="_blank" rel="noopener noreferrer"><img className="project-icon" src="https://img.icons8.com/ios-filled/50/000000/link--v1.png" alt="Live Application" /></a>
