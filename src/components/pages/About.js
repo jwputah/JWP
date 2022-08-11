dc981fdd1e31314ac9df5373af2a133653d72ef1
jwputah <jwputah7@gmail.com>
2022-08-11 13:00:54 -0600

    about page background img

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import '../../assets/css/about.css';
 import photo from '../../../src/assets/images/aboutpic.jpg';
 
 const aboutBg = require("../../../src/assets/images/aboutbg.jpg");
 const divStyle = {
-    height: '823px',
+    height: '1150px',
     width: '100%',
     backgroundImage: `url(${aboutBg})`,
     backgroundSize: 'cover'
@@ -15,21 +12,21 @@ const divStyle = {
 
 export default function About() {
     return (
-        <Container fluid className='background' style={divStyle} >
+        <div className='background' style={divStyle} >
             <div>
-                <Row className='about'>
-                    <Col sm={8} className='aboutContent'>
+                <div className='about'>
+                    <div className='aboutContent'>
                         <h1>
                         Full stack developer from The University of Utah bootcamp, applying tools of the trade CSS, HTML and Javascript. 
                         Combined with years of successful job performance using effective communication within team settings to solve problems equals “ME”. 
                         A creative candidate dedicated to coding best practices, aiming for perfection and helping to make the world a better place with web applications.
                         </h1>
-                    </Col>
-                        <Col sm={4} className='aboutPic'>
+                    </div>
+                        <div className='aboutPic'>
                         <img src={photo} style={{borderRadius: '25px'}} alt="Joshua Pace"/>
-                        </Col>
-                </Row>
+                        </div>
+                </div>
             </div>
-        </Container>
+        </div>
     );
 }
