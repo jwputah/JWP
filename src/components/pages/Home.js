5e926bcf14584bc4f904d199283f1810dc27de06
jwputah <jwputah7@gmail.com>
2022-08-11 12:39:35 -0600

    fixed white space and refactored home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import TypeAnimation from 'react-type-animation';
 import '../../assets/css/home.css';
-// import { Fab, Action } from 'react-tiny-fab';
-import 'react-tiny-fab/dist/styles.css';
 
 const homeBg = require("../../../src/assets/images/homebg.jpg");
 const divStyle = {
     width: '100%',
-    height: '800px',
+    height: '870px',
     backgroundImage: `url(${homeBg})`,
     backgroundSize: 'cover'
 };
@@ -15,28 +13,7 @@ const divStyle = {
 export default function Home() {
     return (
         <div className='Home' style={divStyle}>
-            {/* <Fab
-                position={{ left: 0 }}
-                mainButtonStyles={{ backgroundColor: '#3498db', width: 55, borderRadius: 48 }}
-                actionButtonStyles={{ backgroundColor: '#3498db', width: 35, borderRadius: 28 }}
-                icon='i'
-                alwaysShowTitle={true}
-                text="Links"
-            >
-                <Action
-                    text="Email"
-                    onClick={() => window.open('mailto:jwputah7@gmail.com')}
-                />
-                <Action
-                    text="Github"
-                    onClick={() => window.open('https://github.com/jwputah/')}
-                />
-                <Action
-                    text="LinkedIn"
-                    onClick={() => window.open('https://www.linkedin.com/in/joshua-pace-7200aa227?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B3Nv7vwBqSNKKvsm3skxhSg%3D%3D')}
-                />
-            </Fab> */}
-            <div className='type' style={{ width: '121em', fontSize: '75px' }}>
+            <div className='type' style={{ fontSize: '75px' }}>
                 <TypeAnimation
                     cursor={true}
                     sequence={['Welcome to my Portfolio!', 1500, 'Welcome', 2000, 'My name is Joshua Pace']}
