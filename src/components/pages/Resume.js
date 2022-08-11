0a58c6ccd141d8c35668fb446aee3b9f4c658e64
jwputah <jwputah7@gmail.com>
2022-08-11 10:31:44 -0600

    refactored resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,7 +4,7 @@ import '../../assets/css/resume.css';
 const resumeBg = require("../../../src/assets/images/resumebg.gif");
 const divStyle = {
     width: '100%',
-    height: '823px',
+    height: '870px',
     backgroundImage: `url(${resumeBg})`,
     backgroundSize: 'cover'
 };
@@ -12,23 +12,19 @@ const divStyle = {
 export default function Resume() {
     return (
         <div>
-            <div className='row justify-content-center'>
-                <div className='col'>
-                    <h1 style={{
-                        textAlign: "center",
-                        color: "rgba(35,55,118,255)",
-                        paddingTop: "25px"
-                    }}>Click Here For Resume</h1>
+            <div>
+                <h1 style={{
+                    textAlign: "center",
+                    color: "rgba(35,55,118,255)",
+                    paddingTop: "15px"
+                }}>Click Here For Resume</h1>
 
-                    <div style={{
-                        textAlign: "center",
-                    }}>
-                        <a href="https://docs.google.com/document/d/1DNCw8EnKCy6zX2FNBht4MkKdMegIeoDuWSWuMklixkM/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
-                            <img src="https://img.icons8.com/ios-filled/50/000000/open-resume.png" alt="Resume" className="icon" /></a>
-                    </div>
+                <div style={{ textAlign: "center" }}>
+                    <a href="https://docs.google.com/document/d/1DNCw8EnKCy6zX2FNBht4MkKdMegIeoDuWSWuMklixkM/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+                        <img src="https://img.icons8.com/ios-filled/50/000000/open-resume.png" alt="Resume" className="icon" /></a>
                 </div>
+                <div className='resume' style={divStyle}></div>
             </div>
-            <div className='resume' style={divStyle}></div>
         </div>
     );
 }
